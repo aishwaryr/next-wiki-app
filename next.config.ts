3f8a851b794abeb00b3765841d21464d2986a5aa
Aishwary Raj <araj713@gmail.com>
2026-08-07 18:39:39 +0530

    add turbopack to nextconfig

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,10 @@
 import type { NextConfig } from "next";
+import { dirname } from "node:path";
 
-const nextConfig: NextConfig = {/* config options here */};
+const nextConfig: NextConfig = {
+  turbopack: {
+    root: dirname(__filename),
+  },
+};
 
 export default nextConfig;
